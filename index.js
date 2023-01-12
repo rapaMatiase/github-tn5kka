483ef2385054a55525a74f99c5fe12d4e59ecb37
rapaMatiase <45519553+rapaMatiase@users.noreply.github.com>
2023-01-12 11:19:44 -0300

    4 files modified, 1 deleted

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log("hola")
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+appDiv.innerHTML = `<h1>JS Starter</h1>`;
